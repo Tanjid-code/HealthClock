27ca1a109e32a94c6869f0f15b3e0e25768a48c3
Tanjid <mdtanjidshaik@gmail.com>
2025-01-23 21:31:13 +0600

    HealthClock is a JavaFX-based application that helps users manage their medication schedules. It allows users to upload prescription images or manually input details to set automatic alarms for medicine times. Alarms repeat based on prescription instructions and are automatically deleted after the specified period.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.tanjid.healthclock {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.tanjid.healthclock to javafx.fxml;
+    exports com.tanjid.healthclock;
+}
